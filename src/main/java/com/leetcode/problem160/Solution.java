a5d371b772975351dc7990f5af5e579e576f9d18
Roman Savko <roman.savko@me.com>
2019-12-29 21:34:18 +0200

    Another Solution

--- src/main/java/com/leetcode/problem160/Solution.java
+++ src/main/java/com/leetcode/problem160/Solution.java
@@ -1,7 +1,7 @@
 package com.leetcode.problem160;
 
 public class Solution {
-    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+    public ListNode getIntersectionNode2(ListNode headA, ListNode headB) {
         if (headA == null || headB == null) return null;
 
         int headALength = 0;
@@ -31,5 +31,20 @@ public class Solution {
             longer = longer.next;
         }
         return longer;
+    }
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) return null;
+        ListNode a = headA;
+        ListNode b = headB;
+
+        //if a & b have different len, then we will stop the loop after second iteration
+        while( a != b){
+            //for the end of first iteration, we just reset the pointer to the head of another linkedlist
+            a = a == null? headB : a.next;
+            b = b == null? headA : b.next;
+        }
+
+        return a;
     }
 }
